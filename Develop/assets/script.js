70d2962a096d7b1c26f94ebde1614236b48071bd
ishelly1997 <ishelly1997@gmail.com>
2022-01-23 16:09:56 -0500

    reorg files

---
+++ Develop/assets/script.js
@@ -0,0 +1,17 @@
+//When I open the planner, the current day is displayed at the top of the calendar
+$(document).ready(function() {
+//This function needs to call server to update time and date to show ://WHEN I view the time blocks for that day, each time block is color-coded to indicate whether it is in the past, present, or future
+  })
+
+//WHEN I scroll down, I am presented with time blocks for standard business hours
+//08-4pm this is the bootstrap part 
+
+
+//WHEN I click into a time block, then I can enter an event
+//event listener and save user input 
+
+//WHEN I click the save button for that time block, the text for that event is saved in local storage
+//is this a console log?
+
+//WHEN I refresh the page, the saved events persist
+//append user input to display within the event description 
