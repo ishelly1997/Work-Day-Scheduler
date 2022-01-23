7b3b7636c7ed0c6ffcb07df3b0ed6215ac1dd21a
ishelly1997 <ishelly1997@gmail.com>
2022-01-23 18:47:10 -0500

    added moment.js

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,8 +1,19 @@
 //When I open the planner, the current day is displayed at the top of the calendar
+var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+//console.log(currentDate)
+$("#currentDay").append(currentDay);
+var hoursMinutes = moment();
+//console.log(hoursMinutes)
+var store = window.localStorage;
+
 $(document).ready(function() {
+  
 //This function needs to call server to update time and date to show ://WHEN I view the time blocks for that day, each time block is color-coded to indicate whether it is in the past, present, or future
   })
+  //display current date and time at p lead
+function currentTime() {
 
+}
 //WHEN I scroll down, I am presented with time blocks for standard business hours
 //08-4pm this is the bootstrap part 
 
