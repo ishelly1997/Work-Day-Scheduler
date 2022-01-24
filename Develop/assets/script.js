d66c93031877c517782339579e6f1f54926fc4de
ishelly1997 <ishelly1997@gmail.com>
2022-01-23 22:39:58 -0500

    got the background to correlate with the time

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,47 +1,41 @@
-//When I open the planner, the current day is displayed at the top of the calendar
+//format jmoment
 var currentDay = moment().format('MMMM Do YYYY');
-console.log(currentDay)
+//console.log(currentDay)
 $("#currentDay").append(currentDay);
-var currentTime = moment().format('LT');
-console.log(currentTime)
 
 var eventDetails = document.getElementById("#EVDN");
 
-
 var myStorage = window.localStorage;
 var m = moment();
-//document.createAttribute(textarea)
-//var Textcontent = textarea user.input;
-$(document).ready(function () {
-  
-
-$(".time-block").each(function () {
-  var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-  
-  if (currentTime == blockTime) {
-    $(this).addClass("present");
-  } else if (currentTime < blockTime) {
-    $(this).removeClass("present");
-    $(this).addClass("future");
-  } else (currentTime > blockTime); {
-    $(this).removeClass("future");
-    $(this).addClass("past");
-  }
-});
+//Date and Time Element
 $(document).ready(function() {
   $(".btn-info").on("click", function() {
     var eventDetails =
     $("textarea").append(eventDetails);
-    console.log(eventDetails)
+    //console.log(eventDetails)
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, text);
-  });
-/* Color Time Coordination*/
-/* 
-  $(this).replaceWith(textInput);
-  }) */
-//This function needs to call server to update time and date to show ://WHEN I view the time blocks for that day, each time block is color-coded to indicate whether it is in the past, present, or future
+  })
+//Time Block Coloring Coding Function
+  function runTime() {
+    var currentTime = moment().hour();
+    //console.log(currentTime)
+    $(".timeblock").each(function () {
+      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+  
+      if (currentTime == blockTime) {
+        $(this).addClass("present");
+  } 
+      else if (currentTime < blockTime) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+  }   else (currentTime > blockTime); {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+  }
+})} 
+//Local Storage Items
 $("#hour8 .description").val(localStorage.getItem("hour8"));
 $("#hour9 .description").val(localStorage.getItem("hour9"));
 $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -51,5 +45,5 @@ $("#hour13 .description").val(localStorage.getItem("hour13"));
 $("#hour14 .description").val(localStorage.getItem("hour14"));
 $("#hour15 .description").val(localStorage.getItem("hour15"));
 $("#hour16 .description").val(localStorage.getItem("hour16"));
- 
-})})
+runTime();
+})
